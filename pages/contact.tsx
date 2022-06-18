d42786a9c2bbb44fac52b58a8561b1263445d4b4
Highlander-Maker <highlander1952@protonmail.com>
2022-06-18 20:11:54 +0100

    create pages

---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+function contact() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default contact;
