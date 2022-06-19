13241a369d2b3aa7f9fb65487061d36ef79e1c4a
Highlander-Maker <highlander1952@protonmail.com>
2022-06-19 11:58:01 +0100

    link pages

--- pages/networks.tsx
+++ pages/networks.tsx
@@ -5,6 +5,18 @@ function networks() {
   return (
     <div>
       <Header />
+      <div className="flex justify-between items-center bg-cyan-500 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-y-20">
+          <h1 className="text-6xl max-w-xl font-semibold">
+           Supported Networks
+          </h1>
+          <h2 className="font-semibold">
+           Choose a network you wish to stake to
+          </h2>
+        </div>
+
+       
+      </div>
     </div>
   );
 }
