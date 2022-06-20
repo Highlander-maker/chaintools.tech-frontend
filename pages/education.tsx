fbcc0aa62e4a7c5f1fb939f791e0cf1faddb1107
Highlander-Maker <highlander1952@protonmail.com>
2022-06-20 14:19:25 +0100

    update education page and logos

--- pages/education.tsx
+++ pages/education.tsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { RefreshIcon } from "@heroicons/react/outline";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function education() {
   return (
     <div>
-
-      {/* POSTS GO HERE FROM SANITY */}
-
-      
       <Header />
+
+      <div>
+        <h1>Home</h1>
+        <RefreshIcon className="h-8 w-8" />
+      </div>
       <Footer />
     </div>
   );
