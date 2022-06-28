1e434be9302204b098d1fe834905c4029ea30480
Highlander-Maker <highlander1952@protonmail.com>
2022-06-28 09:40:34 +0100

    networks add layout

--- pages/networks.tsx
+++ pages/networks.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "../components/Header";
+import Layout from '../components/Layout';
 
 function networks() {
   return (
+    <Layout>
     <div>
-      <Header />
       <div className="flex justify-between items-center bg-cyan-500 border-y border-black py-10 lg:py-0">
         <div className="px-10 space-y-20">
           <h1 className="text-6xl max-w-xl font-semibold">
@@ -18,6 +18,7 @@ function networks() {
        
       </div>
     </div>
+    </Layout>
   );
 }
 
