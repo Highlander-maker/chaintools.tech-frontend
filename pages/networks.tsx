9c8686204cd3528b224f3e34ad9f24d6964fcdd4
Highlander-Maker <highlander1952@protonmail.com>
2022-07-01 11:17:30 +0100

    add read more hrefs

--- pages/networks.tsx
+++ pages/networks.tsx
@@ -32,9 +32,11 @@ function networks() {
 
           {/* button style */}
           <div className="px-6 pt-4 pb-2">
+            <a href="https://www.junonetwork.io/">
             <span className="inline-block bg-gray-400 text-black px-4 py-2 text-sm font-semibold mr-2 mb-3 cursor-pointer tracking-widest rounded-full hover:bg-cyan-400">
               Read More
             </span>
+            </a>
           </div>
         </div>
 
