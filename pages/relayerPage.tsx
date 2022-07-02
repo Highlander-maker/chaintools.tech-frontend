07bf05ca99978a6c751603009f0868515ad18ef8
Highlander-Maker <highlander1952@protonmail.com>
2022-07-02 20:36:20 +0100

    add coming soon to relayer page for live site

--- pages/relayerPage.tsx
+++ pages/relayerPage.tsx
@@ -6,8 +6,10 @@ function relayersPage() {
     <Layout>
 
       {/* relayer operations  */}
+      <div >
 
-      <div className="justify-center rounded-lg p-3 grid grid-cols-1 sm:grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <img src="/comingsoon.png" alt="" />
+      {/* <div className="justify-center rounded-lg p-3 grid grid-cols-1 sm:grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
           <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
             <span className="tracking-widest text-lg title-font font-medium text-black mb-1">
@@ -102,7 +104,7 @@ function relayersPage() {
               {" "}
             </span>
           </div>
-        </div>
+        </div> */}
 
         
         
