4b27fc6220e27183cf647e3dd9f7d13f39447e2a
Highlander-Maker <highlander1952@protonmail.com>
2022-07-02 22:21:13 +0100

    add junonetwork.io href

--- pages/networks.tsx
+++ pages/networks.tsx
@@ -33,9 +33,11 @@ function networks() {
 
           {/* button style */}
           <div className="px-6 pt-4 pb-2">
+          <a href="https://www.junonetwork.io/">
             <span className="inline-block bg-gray-400 text-black px-4 py-2 text-sm font-semibold mr-2 mb-3 cursor-pointer tracking-widest rounded-full hover:bg-gray-300">
               Read More
             </span>
+              </a>
           </div>
         </div>
 
