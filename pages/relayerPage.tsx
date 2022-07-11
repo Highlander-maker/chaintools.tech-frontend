25981594c38268177d3bd8fb39ae2163ab5f6399
Highlander-Maker <highlander1952@protonmail.com>
2022-07-11 11:48:06 +0100

    finish design

--- pages/relayerPage.tsx
+++ pages/relayerPage.tsx
@@ -5,7 +5,7 @@ function relayersPage() {
   return (
     <Layout>
       <div className="w-full min-h-screen bg-slate-400 p-6">
-        <h1 className="font-bold text-xl text-center ">
+        <h1 className="font-bold text-xl text-center md:text-3xl md:mt-12 mb-4">
           Checkout the networks we are running{" "}
           <span className="text-red-700"> Relayers </span> on
         </h1>
@@ -22,21 +22,21 @@ function relayersPage() {
           <div className="flex flex-col bg-white rounded -lg  shadow-md w-full m-6 overflow-hidden sm:w-64">
             <img className="h-20 m-6 " src="/juno.svg" alt="" />
             <h2 className="text-center bg-gray-300 hover:bg-red-300 transition-all duration-500 px-2 font-extrabold pb-5 ">
-              Juno - Kujira
+            Omniflix - Kichain
             </h2>
           </div>
 
           <div className="flex flex-col bg-white rounded -lg  shadow-md w-full m-6 overflow-hidden sm:w-64">
             <img className="h-20 m-6 " src="/juno.svg" alt="" />
             <h2 className="text-center bg-gray-300 hover:bg-red-300 transition-all duration-500 px-2 font-extrabold pb-5 ">
-              Juno - Kujira
+              Juno - Bitsong
             </h2>
           </div>
 
           <div className="flex flex-col bg-white rounded -lg  shadow-md w-full m-6 overflow-hidden sm:w-64">
             <img className="h-20 m-6 " src="/juno.svg" alt="" />
             <h2 className="text-center bg-gray-300 hover:bg-red-300 transition-all duration-500 px-2 font-extrabold pb-5 ">
-              Juno - Kujira
+              Sentinel - Decentr
             </h2>
           </div>
 
