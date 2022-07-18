0da49a93a1ea2ef5e9f93d1405cbbe1833cac891
Highlander-Maker <highlander1952@protonmail.com>
2022-07-18 11:17:17 +0100

    add relayer hubs

--- pages/relayerPage.tsx
+++ pages/relayerPage.tsx
@@ -6,14 +6,69 @@ function relayersPage() {
     <Layout>
 
       {/* relayer operations  */}
-      <div >
+     
 
-        <img src="/comingsoon.png" alt="" />
-      {/* <div className="justify-center rounded-lg p-3 grid grid-cols-1 sm:grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="justify-center rounded-lg p-3 grid grid-cols-1 sm:grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
+
+          <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
+            <img src="/Juno-bitsong.jpeg" alt="" />
+            <span className="justify-center flex-auto tracking-widest text-lg title-font font-medium text-black mb-1">
+              {"Juno > Bitsong "}
+            </span>
+          </div>
+        </div>
+
         <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
           <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
+            <img src="/juno-kuji.jpeg" alt="" />
             <span className="tracking-widest text-lg title-font font-medium text-black mb-1">
-              {" "}
+              {"Juno > Kujira "}
+            </span>
+          </div>
+        </div>
+
+        <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
+          <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
+          <img src="/juno-ki.jpeg" alt="" />
+            <span className="tracking-widest text-lg title-font font-medium text-black mb-5">
+              {" Juno > Ki-chain "}
+            </span>
+          </div>
+        </div>
+
+        <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
+          <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
+           <img src="/rizon-ki.jpeg" alt="" />
+            <span className="tracking-widest text-lg title-font font-medium text-black mb-1">
+              {"Rizon - Ki-Chain"}
+            </span>
+          </div>
+        </div>
+
+        <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
+          <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
+          <img src="/flix-ki.jpeg" alt="" />
+            <span className="tracking-widest text-lg title-font font-medium text-black mb-1">
+              {" Omniflix - Ki-Chain"}
+            </span>
+          </div>
+        </div>
+
+        <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
+          <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
+          <img src="/flix-sent.jpeg" alt="" />
+            <span className="tracking-widest text-lg title-font font-medium text-black mb-5">
+              {" Omniflix - Sentinel "}
+            </span>
+          </div>
+        </div>
+
+        <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
+          <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
+          <img src="/sent-decentr.jpeg" alt="" />
+            <span className="tracking-widest text-lg title-font font-medium text-black mb-1">
+              {" Sentinel - Decentr "}
             </span>
           </div>
         </div>
@@ -57,54 +112,6 @@ function relayersPage() {
             </span>
           </div>
         </div>
-
-        <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
-          <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
-            <span className="tracking-widest text-lg title-font font-medium text-black mb-1">
-              {" "}
-            </span>
-          </div>
-        </div>
-
-        <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
-          <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
-            <span className="tracking-widest text-lg title-font font-medium text-black mb-1">
-              {" "}
-            </span>
-          </div>
-        </div>
-
-        <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
-          <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
-            <span className="tracking-widest text-lg title-font font-medium text-black mb-5">
-              {" "}
-            </span>
-          </div>
-        </div>
-
-        <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
-          <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
-            <span className="tracking-widest text-lg title-font font-medium text-black mb-1">
-              {" "}
-            </span>
-          </div>
-        </div>
-
-        <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
-          <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
-            <span className="tracking-widest text-lg title-font font-medium text-black mb-1">
-              {" "}
-            </span>
-          </div>
-        </div>
-
-        <div className="bg-gray-200 max-w-sm rounded-2xl mx-5 overflow-hidden shadow-lg">
-          <div className=" py-4 sm:h-40 md:h-40 lg:h-40">
-            <span className="tracking-widest text-lg title-font font-medium text-black mb-5">
-              {" "}
-            </span>
-          </div>
-        </div> */}
 
         
         
