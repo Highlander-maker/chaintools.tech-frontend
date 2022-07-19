ec7c494356278122866cec4dce7e548dc6fb6e13
Highlander-Maker <highlander1952@protonmail.com>
2022-07-19 13:28:22 +0100

    fix layout and add statesync page

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <Header />
 
       <div className="flex-auto justify-between items-center border-black py-10 lg:py-20">
-        <img className="object-cover fixed bottom-10 mb-5 mix-blend-overlay" src="/background.svg" alt="" />
+        <img className="object-fill fixed mix-blend-overlay md:object-fill" src="/background.svg" alt="" />
         <div className="px-10 space-y-40">
           <h1 className="text-slate-300 text-5xl max-w-xl font-semibold">
             <p className="text-black font-bold text-6xl">
